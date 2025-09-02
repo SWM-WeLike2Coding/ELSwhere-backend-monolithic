1ca4d9156196e79ef563faeedc4465369d3a6267
kjungw1025 <kjungw1025@naver.com>
2025-09-03 02:15:47 +0900

    refactor: Migrate ProductTickerSymbolRepository from Java to Kotlin

--- src/main/kotlin/com/wl2c/elswhere/domain/product/repository/ProductTickerSymbolRepository.kt
+++ src/main/kotlin/com/wl2c/elswhere/domain/product/repository/ProductTickerSymbolRepository.kt
@@ -2,6 +2,12 @@ package com.wl2c.elswhere.domain.product.repository
 
 import com.wl2c.elswhere.domain.product.model.entity.ProductTickerSymbol
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface ProductTickerSymbolRepository: JpaRepository<ProductTickerSymbol, Long> {
+
+    @Query("select p from ProductTickerSymbol p " +
+            "where p.product.id = :productId ")
+    fun findAllByProductId(@Param("productId") productId: Long): List<ProductTickerSymbol>
 }
